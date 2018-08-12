cc1a84d91494e0fa00ff1ec64026fd09a59af037
philzook58 <philzook58@gmail.com>
2018-08-11 23:33:53 -0400

    New version. More efficient, doesn't need V annotation, but haven't reimplemented the embedded morphisms. Should try carrying through the type parameters

--- src/CCC.hs
+++ src/CCC.hs
@@ -1,5 +1,118 @@
+{-# LANGUAGE DataKinds, 
+    AllowAmbiguousTypes, 
+    TypeFamilies, 
+    TypeOperators, 
+    MultiParamTypeClasses, 
+    FunctionalDependencies, 
+    PolyKinds, 
+    FlexibleInstances, 
+    UndecidableInstances,
+    TypeApplications,
+    NoImplicitPrelude,
+    ScopedTypeVariables #-}
 module CCC where
 
 import Cat
+import Data.Proxy 
+import Data.Type.Equality (type (==))
+import Prelude (Bool(..))
+import Control.Category
+
+
+-- The unfortunate Incoherent instances I need to force polymorphic values
+class IsTup a b | a -> b
+instance {-# INCOHERENT #-} (c ~ 'True) => IsTup (a,b) c
+instance {-# INCOHERENT #-} (b ~ 'False) => IsTup a b
+
+data Leaf n = Leaf
+
+
+data Z
+data S a
+
+
+data App f a = App f a
+
+f $$ x = App f x
+
+
+class Cartesian k => CCC a k a' b' | a a' -> b' where
+   ccc :: a -> k a' b'
+instance (Tag a n,
+         Build a b a' b',
+         Cartesian k)
+    => CCC (a->b) k a' b' where
+        ccc f = build @a @b @a' @b'  -- build (Proxy :: Proxy labels) (Proxy :: Proxy b) res where
+                --res = f (val (Proxy :: Proxy a) (Proxy :: Proxy Z) (Proxy :: Proxy c))
+
+class Tag a totaln | a -> totaln where
+    val :: a
+
+instance (IsTup a flag, Tag' a Z flag n) => Tag a n where
+    val = val' @a @Z @flag
+
+class Tag' a n (flag :: Bool) n'| a n flag -> n' where
+    val' :: a
+
+instance (IsTup a flaga,
+          IsTup b flagb,
+          Tag' a n flaga n'',
+          Tag' b n'' flagb n') => Tag' (a,b) n 'True n'  where
+    val' = (val' @a @n @flaga, val' @b @n'' @flagb)
+
+instance (a ~ Leaf n) => Tag' a n 'False (S n)  where
+    val' = Leaf
+
+
+
+type family Or a b where
+    Or 'True b = 'True
+    Or a 'True = 'True
+    Or 'False 'False = 'False
+
+class In a b flag | a b -> flag where
+instance (
+    ((a,b) == c) ~ isc, 
+    flag' ~ Or flaga flagb, 
+    flag ~ Or flag' isc,
+    In a c flaga, 
+    In b c flagb) => In (a,b) c flag
+instance ((Leaf n == c) ~ flag) => In (Leaf n) c flag
+
+
+class Build input key a' b' | input key a' -> b' where
+   build :: Cartesian k => k a' b'
+
+instance ( 
+    iseq ~ ((a,b) == key),
+    In a key isinleft,
+    In b key isinright,
+    Cond iseq isinleft isinright (a,b) key a' b'
+    ) => Build (a,b) key a' b' where
+    build = cond @iseq @isinleft @isinright @(a,b) @key @a'
+
+instance (Leaf a ~ b, a' ~ b') => Build (Leaf a) b a' b' where
+    build = id
+
+
+class Cond iseq isinleft isinright input key a b | iseq isinleft isinright input key a -> b where
+    cond :: Cartesian k => k a b
+instance (a ~ b) => Cond 'True x x input key a b where
+    cond = id
+instance Build a key a' c' => Cond 'False 'True x (a,b) key (a',b') c' where
+    cond = (build @a @key @a') . fst
+instance Build b key b' c' => Cond 'False 'False 'True (a,b) key (a',b') c' where
+    cond = (build @b @key @b') . snd
+instance (Build input key1 a' c', 
+          Build input key2 a' d',
+          key ~ (key1,key2)) => Cond 'False 'False 'False input key a' (c',d') where
+    cond = fan (build @input @key1 @a') (build @input @key2 @a')
+
+
+
+
+
+
+
 
 
--- src/Cat.hs
+++ src/Cat.hs
@@ -1,48 +1,50 @@
-{-# LANGUAGE GADTs, StandaloneDeriving  #-}
+{-# LANGUAGE GADTs, StandaloneDeriving, NoImplicitPrelude  #-}
 
 module Cat where
 import Control.Category
-
+import Prelude hiding ((.))
 
 
 class Category k => Monoidal k where
-	par :: k a c -> k b d -> k (a,b) (c,d)
+    par :: k a c -> k b d -> k (a,b) (c,d)
 
 class Monoidal k => Cartesian k where
-	fst :: k (a,b) a 
-	snd :: k (a,b) b 
-	dup :: k a (a,a) 
+    fst :: k (a,b) a 
+    snd :: k (a,b) b 
+    dup :: k a (a,a) 
+
+fan f g = (par f g) . dup
 {-
 data FreeCat k a b where
-	Comp :: FreeCat k b c -> FreeCat k a b -> FreeCat k a c
-	Id :: FreeCat k a a
-	Morph :: k a b -> FreeCat k a b
+    Comp :: FreeCat k b c -> FreeCat k a b -> FreeCat k a c
+    Id :: FreeCat k a a
+    Morph :: k a b -> FreeCat k a b
 
 data FreeMon k a b where
-	Par :: FreeCat a b -> FreeCat c d -> FreeCat (a,c) (b,d)
+    Par :: FreeCat a b -> FreeCat c d -> FreeCat (a,c) (b,d)
 -}
 
 data FreeCat a b where
-	Comp :: FreeCat b c -> FreeCat a b -> FreeCat a c
-	Id :: FreeCat a a
-	Fst :: FreeCat (a,b) a
-	Snd :: FreeCat (a,b) b
-	Dup :: FreeCat a (a,a)
-	Par :: FreeCat a b -> FreeCat c d -> FreeCat (a,c) (b,d)
+    Comp :: FreeCat b c -> FreeCat a b -> FreeCat a c
+    Id :: FreeCat a a
+    Fst :: FreeCat (a,b) a
+    Snd :: FreeCat (a,b) b
+    Dup :: FreeCat a (a,a)
+    Par :: FreeCat a b -> FreeCat c d -> FreeCat (a,c) (b,d)
 
 deriving instance Show (FreeCat a b)
 
 instance Category FreeCat where
-	(.) = Comp
-	id = Id
+    (.) = Comp
+    id = Id
 
 instance Monoidal FreeCat where
-	par = Par
+    par = Par
 
 instance Cartesian FreeCat where
-	fst = Fst
-	snd = Snd
-	dup = Dup
+    fst = Fst
+    snd = Snd
+    dup = Dup
 
 instance Monoidal (->) where
     par f g = \(x,y) -> (f x, g y)  
@@ -50,5 +52,5 @@ instance Monoidal (->) where
 instance Cartesian (->) where
     fst (x,y) = x
     snd (x,y) = y
-    dup x =(x,x)
+    dup x = (x,x)
 
